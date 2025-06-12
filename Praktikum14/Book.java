e30c13599c31f01bf93bb2a4ac8ed0a19d07eefc
nanda ricco <nandaricco2677@gmail.com>
2025-06-12 11:23:58 +0700

    Jobsheet15

---
+++ Praktikum14/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    public String isbn;
+    public String title;
+
+    public Book() {
+
+    }
+
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public String toString() {
+        return "ISBN: " + this.isbn + ", Title: " + this.title;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Book book = (Book) obj;
+        return isbn.equals(book.isbn) && title.equals(book.title);
+    }
+}
